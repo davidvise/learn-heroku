25c739ad55bf41c7563a41c052c99c3f2b3686f9
davidvise <david.vise@mulesoft.com>
2019-05-21 15:09:00 +1000

    Update dummy.js

--- js/dummy.js
+++ js/dummy.js
@@ -21,6 +21,8 @@ formEl.addEventListener('submit', function(event) {
   // This is for the purpose of this demo using jsFiddle AJAX Request endpoint
   formData.append('json', JSON.stringify({example: 'return value'}));
 
+  alert("I am an alert box!");
+  
   // 2. Make the request
   // ================================
   var url = 'http://unitifm.au-s1.cloudhub.io/workorder/retrieve/CHUBB-FMC?wonum=2361105';
